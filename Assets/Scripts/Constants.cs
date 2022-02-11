d2babafaa98a05975735a65b0b21d5967a6115f3
Devilisch <kirowatanabe@gmail.com>
2022-02-11 17:10:28 +0300

    Create Left.Panel, Constants

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,10 @@
+namespace Constants
+{
+    static public class ANIMATION
+    {
+        static public string OPEN_LEFT_PANEL  = "Open Left.Panel";
+        static public string CLOSE_LEFT_PANEL = "Close Left.Panel";
+        static public string OPEN_MODE_PANEL  = "Open StartButton.Layout";
+        static public string CLOSE_MODE_PANEL = "Close StartButton.Layout";
+    }
+}
---
+++ Assets/Scripts/LeftPanel.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Constants;
+
+public class LeftPanel : MonoBehaviour
+{
+    public Animation panelAnimation;
+    public Animation modePanelAnimation;
+    public Button button;
+    public Button startButton;
+    public Button recordsButton;
+    public Button shopButton;
+    public Button exitButton;
+    public Button warmUpModeButton;
+    public Button survivalModeButton;
+    public Button speedrunModeButton;
+    public Button countdownModeButton;
+
+    private bool isPanelActive = false;
+    private bool isModePanelActive = false;
+
+
+
+    private void OnEnable()
+    {
+        button.onClick.AddListener( SwitchPanel );
+        startButton.onClick.AddListener( SwitchModePanel );
+        exitButton.onClick.AddListener( ClosePanel );
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveAllListeners();
+        startButton.onClick.RemoveAllListeners();
+    }
+
+    private void SwitchPanel()
+    {
+        if ( isPanelActive )
+            panelAnimation.Play( ANIMATION.CLOSE_LEFT_PANEL );
+        else
+            panelAnimation.Play( ANIMATION.OPEN_LEFT_PANEL );
+
+        isPanelActive = !isPanelActive;
+    }
+
+    private void SwitchModePanel()
+    {
+        if ( isModePanelActive )
+            modePanelAnimation.Play( ANIMATION.CLOSE_MODE_PANEL );
+        else
+            modePanelAnimation.Play( ANIMATION.OPEN_MODE_PANEL );
+
+        isModePanelActive = !isModePanelActive;
+    }
+}
